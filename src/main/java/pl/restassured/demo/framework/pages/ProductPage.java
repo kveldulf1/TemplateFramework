b41e7f7b7e8f53036869886fe7f3b4c52f9cc177
QaAutomationEngineer@Testing.Eu <QaAutomationEngineer@Testing.Eu>
2024-08-12 19:05:44 +0200

    Refactor getTotalUpfrontPrice

--- src/main/java/pl/restassured/demo/framework/pages/ProductPage.java
+++ src/main/java/pl/restassured/demo/framework/pages/ProductPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.List;
@@ -12,10 +13,7 @@ import java.util.concurrent.TimeUnit;
 public class ProductPage extends BasePage {
 
     @FindBy(css = "button[data-qa='PRD_AddToBasket']")
-    private WebElement addToCartButton;
-
-//    @FindBy(xpath = "//body/div[@id='app']/div[@id='osAppInnerContainer']/main[1]/section[1]/section[1]/div[1]/span[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]")
-//    private WebElement upfrontPrice;
+    private List<WebElement> addToCartButtonList;
 
     @FindBy(xpath = "//body/div[@id='app']/div[@id='osAppInnerContainer']/main[1]/section[1]/section[1]/div[1]/span[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]")
     private WebElement upfrontPrice;
@@ -25,16 +23,15 @@ public class ProductPage extends BasePage {
 
     public ProductPage(WebDriver driver) {
         super(driver);
-
     }
 
     public void clickAddToCartButtonUsingJS() {
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", addToCartButton);
+        wait.until(ExpectedConditions.elementToBeClickable(addToCartButtonList.get(1)));
+        addToCartButtonList.get(1).click();
     }
 
     public String getTotalUpfrontPrice() {
-        wait.until(ExpectedConditions.visibilityOf(upfrontPriceList.get(1)));
+        wait.until(driver -> upfrontPriceList.size() == 2);
         return upfrontPriceList.get(1).getText();
     }
 }
